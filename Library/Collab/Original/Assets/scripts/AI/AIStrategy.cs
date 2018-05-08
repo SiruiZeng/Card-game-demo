a526dd86745fe85b06f8621d83f354faa780f187
SiruiZeng <ray.tsangcn@gmail.com>
2018-05-08 13:54:04 +0800

    demo
    all asset

---
+++ Library/Collab/Original/Assets/scripts/AI/AIStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+	abstract public class AStrategy
+	{
+		public abstract void strategyInterface();
+
+	}
+
+
+
